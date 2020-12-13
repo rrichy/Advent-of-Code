8970583d7ebefc3257204569e2eb1593efe76d71
Richeese <rricheese@users.noreply.github.com>
2020-12-13 10:40:20 +0800

    day10

--- day9.js
+++ day9.js
@@ -1,66 +1,46 @@
-function getInitialAcc(){
-	const commands = $('pre').innerText.trim().split('\n');
+function findXMASWeakness(preamble){
+	const text = $('pre').innerText.trim().split('\n').map(Number);
+	let weakness, //[val, index]
+		encWeakness;
 
-	let accumulator = 0,
-		pastCommands = new Set(),
-		counter = 0;
+	function sumExist(arr, sum){
+		let set = new Set();
 
-	while (!pastCommands.has(counter)){
-		pastCommands.add(counter);
+		for(let i of arr){
+			if(set.has(sum - i)){
+				return true;
+			}
 
-		let instruction = commands[counter].match(/(acc|jmp|nop) ([+-]\d+)/);
-		
-		switch (instruction[1]){
-			case 'acc':
-				accumulator += parseInt(instruction[2]);
-				counter++;
-				break;
-			case 'jmp':
-				counter += parseInt(instruction[2]);
-				break;
-			case 'nop':
-				counter++;
-				break;
+			set.add(i)
+		}
+		return false;
+	}
+
+	for(let i = 0; i < text.length; i++){
+		if(!sumExist(text.slice(i, i + preamble), text[i + preamble])){
+			weakness = [text[i + preamble], i + preamble];
+			break;
 		}
 	}
 
-	return accumulator;
-}
+	for(let i = 0; i < weakness[1]; i++){
+		let size = 1, sum = text[i];
 
-function getRepairedAcc(){
-	const commands = $('pre').innerText.trim().split('\n');
-	
-	let accumulator = 0,
-		pastCommands = [],
-		lastJumpStat, counter = 0;
-		
-	while (counter < commands.length){
-		let instruction = commands[counter].match(/(acc|jmp|nop) ([+-]\d+)/);
-		
-		if (!pastCommands.includes(counter))
-			pastCommands.push(counter);
-		else{
-			[counter, accumulator, pastCommands, lastJumpStat] = lastJumpStat;
-			instruction[1] = 'nop';
+		while(sum < weakness[0]){
+			size++;
+			sum = text.slice(i, i + size).reduce((a, b) => a + b, 0);
 		}
-		
-		switch (instruction[1]){
-			case 'acc':
-				accumulator += parseInt(instruction[2]);
-				counter++;
-				break;
-			case 'jmp':
-				lastJumpStat = [counter, accumulator, pastCommands, lastJumpStat];
-				counter += parseInt(instruction[2]);
-				break;
-			case 'nop':
-				counter++;
-				break;
+
+		if(sum == weakness[0]){
+			encWeakness = Math.min(...text.slice(i, i + size)) + Math.max(...text.slice(i, i + size));
+			break;
 		}
 	}
-	
-	return accumulator;
+
+	return [weakness[0], encWeakness];
 }
 
-console.log('Part 1:', getInitialAcc());
-console.log('Part 2:', getRepairedAcc());
+const weak = findXMASWeakness(25);
+
+console.log('XMAS Weakpoint:', weak[0]);
+console.log('XMAS Encryption weakness:', weak[1]);
