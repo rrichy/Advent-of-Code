7b205e0ed99a9f6e39f6d9c09dedb1b9d5b27749
Richeese <rricheese@users.noreply.github.com>
2020-12-13 12:02:33 +0800

    day 1-2

---
+++ day1.js
@@ -0,0 +1,26 @@
+const expenses = $('pre').innerText.trim().split('\n').map(Number);
+
+function findValuesWithSum(arr, sum, numVals = 2){
+    let set = new Set();
+
+    for(let i of arr){
+        if(numVals == 2){
+            if(set.has(sum - i)){
+                return [i, sum - i];
+            }
+        }
+
+        else{
+            let root = findValuesWithSum(Array.from(set), sum - i, numVals - 1);
+
+            if(root){
+                return [...root, i];
+            }
+        }
+        set.add(i)
+    }
+    return false;
+}
+
+console.log('Part 1:', findValuesWithSum(expenses, 2020, 2).reduce((a, b) => a * b));
+console.log('Part 2:', findValuesWithSum(expenses, 2020, 3).reduce((a, b) => a * b));
