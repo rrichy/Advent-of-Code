59ac77e2c6b073db143d33e40d6fc41af525208a
Richeese <rricheese@users.noreply.github.com>
2020-12-23 09:57:35 +0800

    day3 so clean

---
+++ day3.js
@@ -0,0 +1,17 @@
+const input = require('fs').readFileSync('day3-input.txt').toString().trim();
+
+function tobogganRun(right, down){
+    let area = input.split('\r\n');
+    let [x, y] = [0, 0], trees = 0;
+
+    while(true){
+        x = (x + right) % area[0].length;
+        y += down;
+
+        if(y >= area.length) return trees;
+        if(area[y][x] == '#') trees++;        
+    }
+}
+
+console.log('Part 1:', tobogganRun(3,1));
+console.log('Part 2:', [[1,1], [3,1], [5,1], [7,1], [1,2]].reduce((basket, value) => basket * tobogganRun(...value), 1));
