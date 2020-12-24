043f0d493c2f54aada80a6bee6bce685fb3d27b9
Richeese <rricheese@users.noreply.github.com>
2020-12-24 19:51:56 +0800

    day20
    shows matrix of tiles. todo orientation of each tiles

--- day20.js
+++ day20.js
@@ -1,44 +1,12 @@
 const tilesList = require('fs').readFileSync('day20-sample.txt').toString().trim();
-let tiles = [];
-
-getTileEdges();
-
-function getTileEdges(){
-    for(let tile of tilesList.split('\r\n\r\n')){
-        let [, key, image] = tile.match(/^.*?(\d+):([\r\n.#]*)/);
-
-        let temp = new Tile(key, image);
-        
-        tiles.push(temp);
-    }
-}
-
-function getCorners(){
-    let corners = [];//image corner tiles has 2 edges with no match,
-
-    for(let i = 0; i < tiles.length; i++){
-        let subtiles = tiles.slice(0, i).concat(tiles.slice(i + 1)), numMatch = 0;
-
-        tiles[i].edges.slice(0,4).forEach(edge => {
-            for(let subtile of subtiles){
-                if(subtile.includes(edge)){
-                    numMatch++;
-                    break;
-                }
-            }
-        });
-
-        if(numMatch == 2) corners.push(parseInt(tiles[i].tile));
-    }
-
-    return corners.reduce((a, b) => a * b);
-}
+let tiles = [], image;
 
 class Tile{
     constructor(tileNum, tileStrng){
         this.tile = tileNum;
         this.edges = [];
         this.border = [];
+        this.inplace = 0;
 
         let temp = tileStrng.split('\r\n').map(a => a.split(''));
         
@@ -50,9 +18,71 @@ class Tile{
     }
 }
 
+getTilesArrangement();
+
+function getTilesArrangement(){
+    for(let tile of tilesList.split('\r\n\r\n')){
+        let [, key, image] = tile.match(/^.*?(\d+):([\r\n.#]*)/), 
+            temp = new Tile(key, image.trim());
+
+        tiles.push(temp);
+    }
+
+    for(let i = 0; i < tiles.length; i++){
+        let subtiles = tiles.slice(0, i).concat(tiles.slice(i + 1));
+
+        tiles[i].edges.slice(0,4).forEach(edge => {
+            for(let subtile of subtiles){
+                if(subtile.edges.includes(edge)){
+                    tiles[i].border.push(subtile.tile);
+                    break;
+                }
+            }
+        });
+    }
+
+    let grid = Math.sqrt(tiles.length);
+    image = Array(grid).fill().map(_=>[]);
+
+    for(let row = 0; row < grid; row++){
+        for(let col = 0; col < grid; col++){
+            let temp;
+
+            if(col == 0 && row == 0){
+                temp = tiles.find(a => a.border.length == 2);
+            }
+            else{
+                let ref = [image[row][col - 1], row != 0 ? image[row - 1][col] : undefined];
+                
+                if(ref[0] && ref[1]){
+                    temp = tiles.find(a => a.border.includes(ref[0].tile) && a.border.includes(ref[1].tile) && a.inplace == 0);
+                }
+                else if(ref[0]){
+                    temp = tiles.find(a => a.border.includes(ref[0].tile) && (a.border.length == 3 || a.border.length == 2) && a.inplace == 0);
+                }
+                else{
+                    temp = tiles.find(a => a.border.includes(ref[1].tile) && (a.border.length == 3 || a.border.length == 2) && a.inplace == 0);
+                }
+            }
+
+            temp.inplace = 1;
+            image[row][col] = temp; 
+        }
+    }
+}
+
+function getCorners(){
+    return tiles.filter(a => a.border.length == 2)
+                .map(a => parseInt(a.tile))
+                .reduce((a, b) => a * b);
+}
+
+
+
 //let sub = new Map(tilesEdges);
 //sub.delete('2311');
 
 //console.log(sub);
 
 console.log('Part 1:', getCorners());
+console.log(image);
