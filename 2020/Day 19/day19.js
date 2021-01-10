5d18d64af7e3fde5e825913dccf8fce414bcab7a
Richeese <rricheese@users.noreply.github.com>
2021-01-10 16:34:40 +0800

    fully working

--- 2020/Day 19/day19.js
+++ 2020/Day 19/day19.js
@@ -1,8 +1,13 @@
-const input = require('fs').readFileSync('day19-sample2.txt').toString().trim().split('\r\n\r\n');
-let rules = {};
+const fs = require('fs');
 
-function firstRule(){
-    for(let rule of input[0].split('\r\n')){
+const input = fs.readFileSync('input.txt').toString().trim().split(/\r?\n\r?\n/);
+const sample1 = fs.readFileSync('sample1.txt').toString().trim().split(/\r?\n\r?\n/);
+const sample2 = fs.readFileSync('sample2.txt').toString().trim().split(/\r?\n\r?\n/);
+const sample3 = fs.readFileSync('sample3.txt').toString().trim().split(/\r?\n\r?\n/);
+
+function buildRules(page){
+    let rules = {};
+    for(let rule of page[0].split(/\r?\n/)){
         let [key, val] = rule.split(': ');
 
         if(/[a-z]/.test(val)){
@@ -16,70 +21,36 @@ function firstRule(){
         }
     }
 
-    return input[1].split('\r\n').filter(a => rules[0]().test(a)).length;
+    return rules;
 }
 
-function secondRule(){
-    rules = {};
-
-    for(let rule of input[0].split('\r\n')){
-        let [key, val] = rule.split(': ');
-
-        if(/[a-z]/.test(val)){
-            rules[key] = function() {return new RegExp(val.match(/[a-z]/)[0])};
-        }
-        else if(key == '0'){
-            rules[key] = function() {return new RegExp('^(' + val.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')$')};
-        }
-        else if(key == '8'){
-            rules[key] = function() {return new RegExp('(' + '42 | 42 8'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')};
-        }
-        else if(key == '1'){
-            rules[key] = function() {return new RegExp('(' + '42 31 | 42 11 31'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')};
-        }
-        else{
-            rules[key] = function() {return new RegExp('(' + val.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')};
-        }
-    }
-
-    return input[1].split('\r\n').filter(a => rules[0]().test(a)).length;
+function noAmendment(page, rule){
+    return page[1].split(/\r?\n/).filter(a => rule[0]().test(a)).length;
 }
 
-//rules['8'] = function() {return new RegExp('(' + '42 | 42 8'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')};
-//rules['11'] = function() {return new RegExp('(' + '42 31 | 42 11 31'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')};
+function withAmendment(page){
+    /*
+    say 42: a and 31: b
+    8: 42 | 42 8;            matches a+ ; a, aa, aaa, aaaa, aaaaa
+    11: 42 31 | 42 11 41     matches ab if a & b has same length ; ab, aabb, aaabbb, aaaabbbb  
+    */
+    rules[8] = function() {return new RegExp('(' + rules[42]().source + ')+')}; 
+
+    let regExp = ['(' + rules[42]().source + '){1}(' + rules[31]().source + '){1}', // since regex cannot get the length of any of two groups, i resorted into a finite length match
+        '(' + rules[42]().source + '){2}(' + rules[31]().source + '){2}',
+        '(' + rules[42]().source + '){3}(' + rules[31]().source + '){3}',
+        '(' + rules[42]().source + '){4}(' + rules[31]().source + '){4}',
+        '(' + rules[42]().source + '){5}(' + rules[31]().source + '){5}',
+        '(' + rules[42]().source + '){6}(' + rules[31]().source + '){6}']
 
 
+    rules[11] = function () {return new RegExp(new RegExp('((' + regExp.join(')|(') +'))'))};
 
+    return page[1].split(/\r?\n/).filter(a => rules[0]().test(a)).length;
+}
 
-console.log(firstRule());
-console.log(secondRule());
-/*
-0: 4 1 5 == 
-1: 2 3 | 3 2 == [aaab, aaba, bbab, bbba], [abaa, abbb, baaa, babb]
-2: 4 4 | 5 5 == aa, bb
-3: 4 5 | 5 4 == ab, ba
-4: "a" == return "a"
-5: "b" == return "b"
+let target = input,
+    rules = buildRules(target);
 
-
-let rules = {}
-rules[5] = function() {return new RegExp('b')}
-rules[4] = function() {return new RegExp('a')}
-rules[3] = function() {return new RegExp('(' + '4 5 | 5 4'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')}
-rules[2] = function() {return new RegExp('(' + '4 4 | 5 5'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')}
-rules[1] = function() {return new RegExp('(' + '2 3 | 3 2'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')}
-rules[0] = function() {return new RegExp('(' + '4 1 5'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')}
-
-let rules = {}
-rules['0'] = function() {return new RegExp('(' + '4 1 5'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')}
-rules['1'] = function() {return new RegExp('(' + '2 3 | 3 2'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')}
-rules['2'] = function() {return new RegExp('(' + '4 4 | 5 5'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')}
-rules['3'] = function() {return new RegExp('(' + '4 5 | 5 4'.split(' | ').map(a => a.match(/\d+/g).map(dig => rules[dig]().source).join('')).join('|') + ')')}
-rules['4'] = function() {return new RegExp('a')}
-rules['5'] = function() {return new RegExp('b')}
-
-
-0: 8 11 == 
-8: 42 | 42 8
-11: 42 31 | 42 11 31
-*/
+console.log('Part 1:', noAmendment(target, rules));
+console.log('Part 2:', withAmendment(target, rules));
